9e3ac277a8a922b019ad75a733b1cc02bc16d6ea
Vladislav Ainshtein <v.ainshtein@mercuryo.io>
2022-12-05 14:08:40 +0300

    Login page with two connect buttons

--- Frontend/AdminPanel/AdminPanelApp.py
+++ Frontend/AdminPanel/AdminPanelApp.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
-
+from PIL import Image, ImageTk
 
 class MainController(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Admin Panel")
         self.geometry("800x600")
+        self.minsize(800, 600)
 
         # creating a parent container, which will include all other frames
         parent_container = tk.Frame(self)
@@ -33,15 +34,53 @@ class MainController(tk.Tk):
 
 class LoginPage(tk.Frame):
     def __init__(self, parent, controller):
+        self.controller = controller
         tk.Frame.__init__(self, parent)
-        for i in range(8):
-            self.rowconfigure(i, minsize=100)
         for i in range(6):
-            self.columnconfigure(i, minsize=100)
-        label = ttk.Label(self, text="LoginPage", font=("Inter", 36))
-        label.configure(anchor="center")
-        label.grid(row=1, column=2, columnspan=4, sticky="nsew")
+            self.rowconfigure(i, minsize=100, weight=1)
+        for i in range(8):
+            self.columnconfigure(i, minsize=100, weight=1)
 
+        # self.configure(bg="red")
+        # for rownum in range(6):
+        #     for colnum in range(8):
+        #         if (rownum == 1 and colnum == 2) or (rownum == 1 and colnum == 3) or (rownum == 1 and colnum == 4) or \
+        #                 (rownum == 1 and colnum == 5) or \
+        #            (rownum == 2 and colnum == 3) or (rownum == 2 and colnum == 4) or \
+        #            (rownum == 3 and colnum == 3) or (rownum == 3 and colnum == 4):
+        #             pass
+        #         else:
+        #             l = ttk.Label(self, text=f'{rownum}, {colnum}')
+        #             l.configure(anchor='center')
+        #             l.grid(row=rownum, column=colnum,sticky='nsew', padx=1, pady=1)
+
+        ton_streams = ttk.Label(self, text="TON STREAMS", font=("Inter", 36))
+        ton_streams.configure(anchor="center")
+        ton_streams.grid(row=1, column=2, columnspan=4, sticky="nsew")
+
+        login_info = ttk.Label(self, text="To enter Admin panel connect your wallet", font=("Inter", 16),
+                               wraplength=200, justify="center")
+        login_info.grid(row=2, column=3, columnspan=2, sticky="ns")
+
+        img_tonkooper = ImageTk.PhotoImage(Image.open("images/tonkooper.png"))
+        img_tonkooper_hover = ImageTk.PhotoImage(Image.open("images/tonkooper_hover.png"))
+        login_tonkooper = tk.Label(self, image=img_tonkooper)
+        login_tonkooper.grid(row=3, column=3, columnspan=2)
+        login_tonkooper.image = img_tonkooper
+        login_tonkooper.bind('<Button-1>', lambda e: print("clicked"))
+        login_tonkooper.bind('<Enter>', lambda e: login_tonkooper.configure(image=img_tonkooper_hover))
+        login_tonkooper.bind('<Leave>', lambda e: login_tonkooper.configure(image=img_tonkooper))
+
+        img_looplex = ImageTk.PhotoImage(Image.open("images/looplex.png"))
+        img_looplex_hover = ImageTk.PhotoImage(Image.open("images/looplex_hover.png"))
+        login_looplex = tk.Label(self, image=img_looplex)
+        login_looplex.grid(row=4, column=3, columnspan=2)
+        login_looplex.image = img_looplex
+        login_looplex.bind('<Enter>', func=lambda e: change_background(e, login_looplex, img_looplex_hover))
+        login_looplex.bind('<Leave>', func=lambda e: change_background(e, login_looplex, img_looplex))
+
+        def change_background(event, label_obj, img_obj):
+            label_obj.configure(image=img_obj)
 
 # Driver Code
 app = MainController()
